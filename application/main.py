5e05c3734d859d05614133fa35f887517c125381
Juergen Skrotzky <jorgenvikinggod@gmail.com>
2023-10-08 18:51:40 +0200

    Simplify example

--- application/main.py
+++ application/main.py
@@ -1,49 +1,11 @@
-import secure
 import uvicorn
 from config import settings
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse
-from starlette.exceptions import HTTPException as StarletteHTTPException
 
-app = FastAPI(openapi_url=None)
-
-csp = secure.ContentSecurityPolicy().default_src("'self'").frame_ancestors("'none'")
-hsts = secure.StrictTransportSecurity().max_age(31536000).include_subdomains()
-referrer = secure.ReferrerPolicy().no_referrer()
-cache_value = secure.CacheControl().no_cache().no_store().max_age(0).must_revalidate()
-x_frame_options = secure.XFrameOptions().deny()
-
-secure_headers = secure.Secure(
-    csp=csp,
-    hsts=hsts,
-    referrer=referrer,
-    cache=cache_value,
-    xfo=x_frame_options,
-)
-
-
-@app.middleware("http")
-async def set_secure_headers(request, call_next):
-    response = await call_next(request)
-    secure_headers.framework.fastapi(response)
-    return response
-
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[settings.client_origin_url],
-    allow_methods=["GET"],
-    allow_headers=["Authorization", "Content-Type"],
-    max_age=86400,
-)
-
-
-@app.exception_handler(StarletteHTTPException)
-async def http_exception_handler(request, exc):
-    message = str(exc.detail)
-
-    return JSONResponse({"message": message}, status_code=exc.status_code)
+app = FastAPI(
+    title="My Super Project",
+    description="This is a very fancy project, with auto docs for the API and everything",
+    version="1.0.0",)
 
 
 @app.get("/api/messages/public")
